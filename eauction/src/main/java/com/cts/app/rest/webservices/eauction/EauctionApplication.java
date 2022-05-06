ca2c608f5670515f9237461206722229a3eec7f7
unknown <cogjava563@iiht.tech>
2022-05-06 15:01:00 +0530

    first commit

---
+++ eauction/src/main/java/com/cts/app/rest/webservices/eauction/EauctionApplication.java
@@ -0,0 +1,13 @@
+package com.cts.app.rest.webservices.eauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EauctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EauctionApplication.class, args);
+	}
+
+}
---
+++ eauction/src/test/java/com/cts/app/rest/webservices/eauction/EauctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.app.rest.webservices.eauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EauctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
